56e47df9ef56878dcb36a6014f8d5b5e274701c9
ShreeRaz <ankitt60raz@gmail.com>
2025-12-17 15:10:39 +0545

    Added LogIn form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    
+  };
+
+    return (
+        <form onSubmit={handleSubmit}> 
+            <label>
+                Email:
+                <input type="email" value= {email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            </label>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+export default LoginForm;
